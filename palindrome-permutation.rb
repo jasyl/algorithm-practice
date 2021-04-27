e0fca8eeeead9b2b748094b4cf643c229bac2aab
Jasmine <jasylop@gmail.com>
2021-04-26 21:20:31 -0700

    added some tests

--- palindrome-permutation.rb
+++ palindrome-permutation.rb
@@ -1,4 +1,4 @@
-# Write an efficient function that checks whether any permutation of an input string is a palindrome. 
+# Write an efficient function that checks whether any permutation of an input string can a palindrome. 
 # https://medium.com/algorithms-practice/given-a-string-check-if-it-is-a-permutation-of-a-palindrome-e0469f47761d
 # https://www.geeksforgeeks.org/check-characters-given-string-can-rearranged-form-palindrome/
 
@@ -11,8 +11,22 @@ def palindrome_permutation(string)
     if char_freq.has_key?(char)
       char_freq.delete(char)
     else
-      char_freq.[char] = 1
+      char_freq[char] = 1
     end
   end
   return char_freq.length == 1 || char_freq.empty?
 end
+
+# TEST
+
+s = "ada"
+p palindrome_permutation(s)
+
+s = "racecar"
+p palindrome_permutation(s)
+
+s = "bat"
+p palindrome_permutation(s)
+
+s = "rrrcccrrrcc"
+p palindrome_permutation(s)
